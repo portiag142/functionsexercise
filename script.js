070413c9025bd28ecb177c39e3246d0aea25c6e0
Portia Greene <pd12300@yahoo.com>
2021-10-19 21:38:18 -0400

    completed functionsexercise

---
+++ script.js
@@ -0,0 +1,91 @@
+//1.
+function spookyGhost(){
+    console.log('Boo!');
+}
+spookyGhost();
+
+console.log("=======");
+
+//2.
+function candy (goal){
+    console.log('${goal},${goal, ${goal}, ${goal'); 
+
+    candy('Get Candy');
+    
+
+}
+console.log("=======");
+
+function werewolf (a, b){
+    if (a ==="Full" & b ==="Moon"){
+        console.log('ARH_WOOO!');
+    }
+    console.log("=======");
+}
+//4.
+function halloween(){
+   return 'October 31';
+}
+
+halloween();
+console.log(halloween();)
+
+console.log("=======");
+//5.
+function isEnoughCandy(pieces){
+if ( pieces >=31){
+    return 'true';
+} else (pieces < 31)
+   return 'false';
+
+}
+console.log(isEnoughCandy(35));
+
+console.log("=======");
+///6.
+function candyCounter(num){
+
+    let total = 0;
+    for (n of num) {
+        total = total + n;
+    }
+    return total;
+}
+console.log(candyCounter([1, 2, 3, 4]));
+console.log(candyCounter([100, 200]));
+
+console.log("=======");
+
+
+
+
+
+
+
+//7.
+const hauntedMansion = function (){
+   console.log("Welcome, foolish mortals, to the Haunted Mansion!".');
+ }
+ hauntedMansion();
+
+
+ console.log("=======");
+
+//8.
+ghostbusters = () => 'Who You Gonna Call?'
+console.log(ghostbusters());
+    
+console.log("=======");
+
+//9. NO
+
+//10. yes
+
+//11.
+
+const costumes =[Ghost, Princess, Pirates ];
+
+const upperCostumes = costumes.map(function (parameter){
+    return parameter.toUpperCase();
+});
+console.log(upperCostumes);
